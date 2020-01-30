562374a7a2e3be86421ec0994f0ac772ea6ac3be
hii <hii@hi>
2020-01-29 22:46:44 -0500

    project moved to git

---
+++ mavenproject/src/test/java/p1/class1.java
@@ -0,0 +1,21 @@
+package p1;
+
+import org.testng.annotations.Test;
+
+public class class1 {
+  @Test
+  public void tc001() 
+  {
+	  System.out.println("tcoo1 excuted");
+  }
+  @Test
+  public void tc002() 
+  {
+	  System.out.println("tcoo2 excuted");
+  }
+  @Test
+  public void tc003() 
+  {
+	  System.out.println("tcoo3 excuted");
+  }
+}
---
+++ mavenproject/src/test/java/p2/class2.java
@@ -0,0 +1,21 @@
+package p2;
+
+import org.testng.annotations.Test;
+
+public class class2 {
+	@Test
+	  public void tc004() 
+	  {
+		  System.out.println("tcoo4 excuted");
+	  }
+	  @Test
+	  public void tc005() 
+	  {
+		  System.out.println("tcoo2 excuted");
+	  }
+	  @Test
+	  public void tc006() 
+	  {
+		  System.out.println("tcoo3 excuted");
+	  }
+}
